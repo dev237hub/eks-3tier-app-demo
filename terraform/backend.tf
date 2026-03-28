2b21c4ec36601f0de43a1bd9e49f78a1f8cd83f2
Ubuntu <ubuntu@ip-172-31-16-154.ec2.internal>
2026-03-28 12:33:21 +0000

    chore: clean repo and move terraform backend files

--- terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "backend2023tfbucket"
-    key            = "eks/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "eks-terraform-locks"
-  }
-}
