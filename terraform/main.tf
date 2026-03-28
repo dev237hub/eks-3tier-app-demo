feb199925c9dbfe41388bf6328a0310d3bcfb1f6
Ubuntu <ubuntu@ip-172-31-16-154.ec2.internal>
2026-03-28 15:11:56 +0000

    added eks cluster creation

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,3 +26,50 @@ module "vpc" {
     Project     = "eks-3tier"
   }
 }
+
+#############################################
+# EKS CLUSTER CREATION
+#############################################
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 21.0"
+
+  name               = "three-tier-eks"
+  kubernetes_version = "1.29"
+
+  #############################################
+  # ACCESS
+  #############################################
+  endpoint_public_access = true
+
+  enable_cluster_creator_admin_permissions = true
+
+  #############################################
+  # NETWORKING (USE YOUR VPC)
+  #############################################
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  #############################################
+  # NODE GROUP (IMPORTANT FOR PROJECT)
+  #############################################
+  eks_managed_node_groups = {
+    default = {
+      instance_types = ["t3.small"]
+
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+    }
+  }
+
+  #############################################
+  # TAGS
+  #############################################
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+    Project     = "eks-3tier"
+  }
+}
